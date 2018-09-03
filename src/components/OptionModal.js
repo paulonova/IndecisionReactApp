d8fe240b56792109ebd91a18b104b25a57cd46f0
paulonova <paulonova144@gmail.com>
2018-09-03 16:32:53 +0200

    App styling is done and working

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,11 +6,13 @@ const OptionModal = (props) => (
     <Modal
       isOpen={!!props.selectedOption}
       contentLabel="Select Option"
-      onRequestClose={props.handleModalClose}>
+      onRequestClose={props.handleModalClose}
+      closeTimeoutMS={200}
+      className="modal">
       
-        <h3>Selected option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick = {props.handleModalClose}>Okay</button>
+        <h3 className="modal__title">Selected option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick = {props.handleModalClose}>Okay</button>
     </Modal>
 );
 
